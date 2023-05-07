58eba8f3a82489ee26ac6040572833fbb17de4e1
Shayan Mahnam <shayan.mahnam.uk@gmail.com>
2023-05-07 19:57:04 +0100

    title update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,24 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Head>
+        <title>Shayan Film</title>
+        <link rel="icon" href="/images/logo.png" />
+        <link rel="shortcut icon" href="/images/logo.png" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="/images/logo.png" />
+        <meta name="description" content="This is Shayan Film Full Stack App" />
+      </Head>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 }
